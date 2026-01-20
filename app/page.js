8fc99549ad20c2c01465ebc937c2641b87e70e32
r1f666 <rspcitadel@gmail.com>
2026-01-20 19:49:59 +0300

    upd 2000126

--- app/page.js
+++ app/page.js
@@ -1,16 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+import Clock  from "./components/Clock";
+import WeatherForecast from "./components/WeatherForecast";
 
 export default function Home() {
   return (
-      <div style={{ padding: 50, textAlign: 'center', fontFamily: 'Arial' }}>
-        <h1>🚀 HAPPYBIT РАБОТАЕТ!</h1>
-        <p>Проект успешно задеплоен на Vercel</p>
-        <p>Время: {new Date().toLocaleString()}</p>
-        <div style={{ marginTop: 30, padding: 20, background: '#f0f0f0' }}>
-          <h2>✅ Это точно работает!</h2>
-          <p>Если видишь это - всё ок!</p>
-        </div>
+      <div style={{padding: 50, textAlign: 'center', fontFamily: 'Arial'}}>
+          <h1>🚀 HAPPYBIT РАБОТАЕТ!</h1>
+          <p>Проект успешно задеплоен на Vercel</p>
+          <p>Время: {new Date().toLocaleString()}</p>
+          <div style={{marginTop: 30, padding: 20, background: '#f0f0f0'}}>
+              <Clock/>
+
+          </div>
+                 <WeatherForecast/>
+
       </div>
   );
 }
